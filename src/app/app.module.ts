ad8069ed116bbb30fb072f9965f1866c2b204461
Verchenko Maxim <verchenko1998@gmail.com>
2019-04-24 16:39:50 +0300

    version 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MainBoardComponent } from './main-board/main-board.component';
+import { FormControl } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+  AppComponent,
+  MainBoardComponent,
+
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [ 
+  BrowserModule,
+  FormsModule,
+  ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
